3e7ee3867e795155270b8ab02543fb0be2421fda
Emin Jasarevic <emin@jasarevic.com>
2023-11-06 23:14:13 +0100

    clean up service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,35 +7,16 @@ use Eminos\StatamicTabs\Fieldtypes\TabFieldtype;
  
 class ServiceProvider extends AddonServiceProvider
 {
-
-    public function __construct()
-    {
-        $this->vite['hotFile'] = base_path('vendor/eminos/statamic-tabs/dist/vite.hot');
-
-        parent::__construct(app());
-    }
-
     protected $fieldtypes = [
         TabFieldtype::class,
     ];
 
-    // protected $tags = [
-    //     IconifyTag::class,
-    // ];
-
     protected $vite = [
-        'hotFile' => null, // set in the constructor for reasons
+        'hotFile' => __DIR__ . '/../dist/vite.hot',
         'publicDirectory' => 'dist',
         'input' => [
             'resources/js/statamic-tabs.js',
             'resources/css/statamic-tabs.css'
         ],
     ];
-
-    public function bootAddon()
-    {
-
-        // 
-        
-    }
 }
